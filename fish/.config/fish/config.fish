013165f0e28c30eea5ddd549bc338d9650162e93
常运航 <changyunhang@changyunhangdeMacBook-Air.local>
2026-03-08 13:58:10 +0800

    add tex path to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /opt/homebrew/sbin $PATH
+set -gx PATH /Library/TeX/texbin $PATH
 
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
